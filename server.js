ee803c0b72d9846e841f1ec89e7bbfd8575d8941
Alex Booker <alexbooker@fastmail.im>
2015-10-13 12:18:53 +0100

    installed handlebars dependency

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 import routes from './routes';
 
 const app = express();
+
+
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
