0b525e6a7c6c98f1d7e89c0db73c74af2a3d1f82
Alex Booker <alexbooker@fastmail.im>
2015-10-13 16:59:28 +0100

    added join route

--- routes.js
+++ routes.js
@@ -2,9 +2,13 @@ import express from 'express';
 import { 
   renderHomePage
 } from './controllers/homeController';
+import { 
+  renderJoin
+} from './controllers/usersController';
 
 const router = express.Router();
 
 router.get("/", renderHomePage);
+router.get("/join", renderJoin);
 
 export default router;
