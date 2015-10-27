9de23fb1c1948249e314f985f44c80edee0e4c80
Alex Booker <alexbooker@fastmail.im>
2015-10-27 01:43:39 +0000

    added model spec back

---
+++ tests/models/user_spec.js
@@ -0,0 +1,58 @@
+import chai from "chai";
+import chaiStr from 'chai-string';
+import orm from '../../lib/orm';
+
+chai.use(chaiStr);
+const expect = chai.expect;
+
+describe("user", function() {
+  before(function(done) {
+    orm.logger = false;
+    orm.discover = [__dirname + '/../../models/'];
+    orm.connect("", "", "", {
+      logging: false,
+      dialect: "sqlite"
+    });
+    orm
+      .sequelize
+      .sync({
+        force: true
+      }).then(function() {
+        done();
+      });
+  });
+
+  describe("a valid model", function(done) {
+
+    let createResult;
+
+    const user = {
+      username: "username",
+      email: "email@email.com",
+      password: "password"
+    };
+
+    before(function(done) {
+      orm
+        .models
+        .User
+        .create(user)
+        .then(function(result) {
+          createResult = result;
+          done();
+        });
+    });
+
+    it("returns userId", function() {
+      expect(createResult.dataValues.userId).to.exist;
+    });
+
+    it("does not store the password in plaintext", function() {
+      expect(createResult.dataValues.password).to.not.equal(user.password);
+    });
+
+    it("hashes the password using bcrypt", function() {
+      expect(createResult.dataValues.password).to.startsWith('$2a$');
+    });
+  });
+});
