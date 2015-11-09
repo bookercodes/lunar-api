ae1ba5c0570fed58ec7dab989bbb7e16af16574d
Alex Booker <alexbooker@fastmail.im>
2015-11-09 18:32:01 +0000

    back to mocha...

--- test/lib/utilTest.js
+++ test/lib/utilTest.js
@@ -1,44 +1,44 @@
-const test = require("ava");
-const util = require("../../lib/util.js");
+import util from "../../lib/util.js";
+import chai from "chai";
 
-test("extractErrors should return error message only and not the path",
-  function(t) {
+const assert = chai.assert;
+
+describe("extractErrors", function() {
+  it("should return error message only and not the path",
+    function() {
+      const input = [{
+        message: "\"username\" cannot be empty",
+        path: "username"
+      }];
+      const actual = util.extractErrors(input);
+      const expected = [input[0].message];
+      assert(actual, expected);
+      assert.notOk(actual[0].path);
+    });
+  it("should return one error message per path", function() {
     const input = [{
-      message: "\"username\" cannot be empty",
-      path: "username"
+      message: "\"email\" cannot be empty",
+      path: "email"
+    }, {
+      message: "\"email\" must be valid",
+      path: "email"
     }];
-    const actual = util.extractErrors(input);
     const expected = [input[0].message];
-    t.same(actual, expected);
-    t.notOk(actual[0].path);
-    t.end();
+    const actual = util.extractErrors(input);
+    assert(actual, expected);
   });
 
-test("extractErrors should return one error message per path", function(t) {
-  const input = [{
-    message: "\"email\" cannot be empty",
-    path: "email"
-  }, {
-    message: "\"email\" must be valid",
-    path: "email"
-  }];
-  const expected = [input[0].message];
-  const actual = util.extractErrors(input);
-  t.same(actual, expected);
-  t.end();
-});
+  it("can return more than one error", function() {
+    const input = [{
+      message: "\"email\" cannot be empty",
+      path: "email"
+    }, {
+      message: "\"username\" must be valid",
+      path: "username"
+    }];
 
-test("extractErrors can return more than one error", function(t) {
-  const input = [{
-    message: "\"email\" cannot be empty",
-    path: "email"
-  }, {
-    message: "\"username\" must be valid",
-    path: "username"
-  }];
-
-  const expected = [input[0].message, input[1].message];
-  const actual = util.extractErrors(input);
-  t.same(actual, expected);
-  t.end();
+    const expected = [input[0].message, input[1].message];
+    const actual = util.extractErrors(input);
+    assert(actual, expected);
+  });
 });
