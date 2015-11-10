df06518ab764c230191cd847ff501f90ab68c34b
Alex Booker <alexbooker@fastmail.im>
2015-11-10 12:52:53 +0000

    added further model tests

--- test/model/userTest.js
+++ test/model/userTest.js
@@ -6,48 +6,87 @@ import config from "config";
 chai.use(chaiStr);
 const expect = chai.expect;
 
-before(function() {});
+describe("user model", function() {
 
-describe("create with valid model", function(done) {
-
-  let createResult;
-
-  const user = {
-    username: "username",
-    email: "email@email.com",
-    password: "password"
-  };
-
-  before(function(done) {
+  before(function() {
     db.connect(config.database, config.username, config.password, {
       logging: false,
       dialect: config.dialect
     });
-    db
+  });
+
+  beforeEach(function() {
+    return db
       .connection
       .sync({
         force: true
-      }).then(function() {
-        db
-          .models
-          .User
-          .create(user)
-          .then(function(result) {
-            createResult = result;
-            done();
-          });
       });
   });
 
-  it("should return userId", function() {
-    expect(createResult.dataValues.userId).to.exist;
+  describe("with unavailable field, validateAvailability func", function() {
+    it("should return an error", function() {
+      const user = {
+        username: "username",
+        email: "email@email.com",
+        password: "password"
+      };
+      const promise = db
+        .models
+        .User
+        .create(user)
+        .then(function() {
+          return db
+            .models
+            .User
+            .validateAvailability("username", user.username);
+        });
+      return expect(promise).to.eventually.have.length(1);
+    });
   });
 
-  it("should not store the password in plain text", function() {
-    expect(createResult.dataValues.password).to.not.equal(user.password);
-  });
+  describe("with available field, validateAvailability func",
+    function() {
+      it("should return no errors", function() {
+        const promise = db
+          .models
+          .User
+          .validateAvailability("username", "foo");
+        return expect(promise).to.eventually.have.length(0);
+      });
+    });
 
-  it("should hash the password using bcrypt", function() {
-    expect(createResult.dataValues.password).to.startsWith('$2a$');
+  describe("with valid model, create func", function(done) {
+
+    let createResult;
+
+    const user = {
+      username: "username",
+      email: "email@email.com",
+      password: "password"
+    };
+
+    before(function() {
+      return db
+        .models
+        .User
+        .create(user)
+        .then(function(result) {
+          createResult = result;
+        });
+    });
+
+    it("should return userId", function() {
+      expect(createResult.dataValues.userId).to.exist;
+    });
+
+    it("should not store the password in plain text", function() {
+      expect(createResult.dataValues.password).to.not.equal(
+        user.password);
+    });
+
+    it("should hash the password using bcrypt", function() {
+      expect(createResult.dataValues.password).to.startsWith(
+        '$2a$');
+    });
   });
 });
