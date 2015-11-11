14dbd6f798d39d7f17ed985d5549e4c90e81a56d
Alex Booker <alexbooker@fastmail.im>
2015-11-11 18:03:08 +0000

    added more util test coverage

--- test/unit/lib/utilTest.js
+++ test/unit/lib/utilTest.js
@@ -1,14 +1,29 @@
 import sut from "../../../lib/util";
 import chai from "chai";
-import _ from "lodash";
 
-const assert = chai.assert;
+const expect = chai.expect;
 
 suite("util", function() {
 
   test("exports module", function() {
-    assert(!_.isEmpty(sut));
+    expect(sut)
+      .to
+      .not
+      .be
+      .empty;
   });
+
+  test("extractErrors() returns empty array if input is empty array",
+    function() {
+      const input = [];
+
+      const actual = sut.extractErrors(input);
+
+      expect(actual)
+        .to
+        .be
+        .empty;
+    });
 
   test("extractErrors() returns array", function() {
     const input = [{
@@ -18,53 +33,98 @@ suite("util", function() {
 
     const actual = sut.extractErrors(input);
 
-    assert.isArray(actual);
+    expect(actual)
+      .to
+      .be
+      .instanceof(Array);
   });
 
-  test("extractErrors() returns unmodified error message", function() {
-    const message = "\"username\" cannot be empty";
+  test("extractErrors() returns unmodified error message and path",
+    function() {
+      const input = [{
+        message: "\"username\" cannot be empty",
+        path: "username"
+      }];
+
+      const actual = sut.extractErrors(input);
+
+      const expected = [{
+        path: "username",
+        message: "\"username\" cannot be empty"
+      }];
+
+      expect(actual)
+        .to
+        .eql(expected);
+    });
+
+  test("extractErrors() returns one error per path", function() {
     const input = [{
-      message: message,
+      message: "\"username\" cannot be empty",
+      path: "username"
+    }, {
+      message: "\"username\" cannot be foo",
       path: "username"
     }];
 
     const actual = sut.extractErrors(input);
 
-    const expected = [message];
-    assert.deepEqual(actual, expected);
-    assert.notOk(expected[0].path);
-  });
-
-  test("extractErrors() returns just one error per path", function() {
-    const message = "\"email\" cannot be empty";
-    const input = [{
-      message: message,
-      path: "email"
-    }, {
-      message: "\"email\" must be valid",
-      path: "email"
+    const expected = [{
+      message: "\"username\" cannot be empty",
+      path: "username"
     }];
-
-    const actual = sut.extractErrors(input);
-
-    const expected = [message];
-    assert.deepEqual(actual, expected);
+    expect(actual)
+      .to
+      .eql(expected);
   });
 
   test("extractErrors() can return many errors", function() {
-    const message1 =  "\"email\" cannot be empty";
-    const message2 =  "\"username\" must be valid";
     const input = [{
-      message: message1,
-      path: "email"
-    }, {
-      message: message2,
+      message: "\"username\" cannot be empty",
       path: "username"
+    }, {
+      message: "\"email\" cannot be empty",
+      path: "email"
     }];
 
     const actual = sut.extractErrors(input);
 
-    const expected = [message1, message2];
-    assert.deepEqual(actual, expected);
+    const expected = [{
+      path: "username",
+      message: "\"username\" cannot be empty"
+    }, {
+      path: "email",
+      message: "\"email\" cannot be empty"
+    }];
+    expect(actual)
+      .to
+      .eql(expected);
   });
+
+  test("extractErrors() returns correct result", function() {
+    const input = [{
+      message: "\"username\" cannot be empty",
+      path: "username"
+    }, {
+      message: "\"username\" cannot be foo",
+      path: "username"
+    }, {
+      message: "\"email\" cannot be empty",
+      path: "email"
+    }];
+
+    const actual = sut.extractErrors(input);
+
+    const expected = [{
+      message: "\"username\" cannot be empty",
+      path: "username"
+    }, {
+      message: "\"email\" cannot be empty",
+      path: "email"
+    }];
+    expect(actual)
+      .to
+      .eql(expected);
+  });
+
 });
